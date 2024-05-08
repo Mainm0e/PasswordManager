34de2632fba43d20c1ad8b1ab965981c0c71edc6
TheGitGuru <mainm0e1996@gmail.com>
2024-05-08 17:05:05 +0300

    update database

--- application/data/data.go
+++ application/data/data.go
@@ -7,18 +7,22 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var (
+	dataBasePath = "./accounts.db"
+)
+
 func Data() {
 	// Create a database
 	createDataBase()
 	// Insert data into the database
-	insertData()
+	registerAccount("exampleUsername", "examplePassword")
 	// Retrieve data from the database
 	retrieveData()
 }
 
 func createDataBase() {
 	// Open a SQLite database connection
-	db, err := sql.Open("sqlite3", "./passwords.db")
+	db, err := sql.Open("sqlite3", dataBasePath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,7 +30,7 @@ func createDataBase() {
 
 	// SQL statement to create the passwords table
 	sqlStmt := `
-	CREATE TABLE IF NOT EXISTS passwords (
+	CREATE TABLE IF NOT EXISTS accounts (
 		id INTEGER PRIMARY KEY,
 		username TEXT,
 		password TEXT
@@ -37,32 +41,47 @@ func createDataBase() {
 	if err != nil {
 		log.Fatalf("%q: %s\n", err, sqlStmt)
 	}
+
+	// SQL statement to create the applications table
+	sqlStmtApplications := `
+	CREATE TABLE IF NOT EXISTS applications (
+		id INTEGER PRIMARY KEY,
+		account_id INTEGER,
+		application TEXT,
+		application_username TEXT,
+		application_password TEXT,
+		FOREIGN KEY(account_id) REFERENCES accounts(id)
+	);
+	`
+	// Execute the SQL statement to create the applications table
+	_, err = db.Exec(sqlStmtApplications)
+	if err != nil {
+		log.Fatalf("%q: %s\n", err, sqlStmtApplications)
+	}
 }
 
-func insertData() error {
+func registerAccount(username string, password string) error {
 	// Open a SQLite database connection
-	db, err := sql.Open("sqlite3", "./passwords.db")
+	db, err := sql.Open("sqlite3", dataBasePath)
 	if err != nil {
 		log.Fatal(err)
 		return err
 	}
 	defer db.Close() // Make sure to close the database connection when the function ends
 
-	// SQL statement to insert data into the passwords table
+	// SQL statement to insert data into the accounts table
 	sqlStmt := `
-	INSERT INTO passwords (username, password) VALUES ('user1', 'password1');
+	INSERT INTO accounts (username, password) VALUES (?, ?)
 	`
 	// Execute the SQL statement
-	_, err = db.Exec(sqlStmt)
+	_, err = db.Exec(sqlStmt, username, password)
 	if err != nil {
 		log.Fatalf("%q: %s\n", err, sqlStmt)
 		return err
 	}
 
 	return nil
-
 }
-
 func retrieveData() {
 
 }
--- application/data/data_test.go
+++ application/data/data_test.go
@@ -1,25 +1,16 @@
 package data
 
 import (
-	"os/exec"
 	"testing"
 )
 
 func TestData(t *testing.T) {
 	// Create a database
-	//createDataBase()
+	createDataBase()
 	// check if the database is created
 
 	// try to insert data into the database
-	err := insertData()
-	if err != nil {
-		t.Error("Error inserting data")
-	}
-	// delete password.db
-
-	cmd := exec.Command("rm", "passwords.db")
-	err = cmd.Run()
-
+	err := registerAccount("exampleUsername", "examplePassword")
 	if err != nil {
 		t.Error("Error inserting data")
 	}
